56c8cf887f32e2353ab8f1012d0c28b194c21b5a
Kiran Shila <me@kiranshila.com>
2022-05-19 13:50:51 -0700

    Start rewrite of stopwatch with chrono

--- Pipeline/hd/stopwatch.hpp
+++ Pipeline/hd/stopwatch.hpp
@@ -1,21 +1,11 @@
-/***************************************************************************
- *
- *   Copyright (C) 2012 by Ben Barsdell and Andrew Jameson
- *   Licensed under the Academic Free License version 2.1
- *
- ***************************************************************************/
-
 #ifndef STOPWATCH_HPP
 #define STOPWATCH_HPP
 
-// includes, system
-#include <ctime>
-#include <sys/time.h>
+#include <chrono>
 
-// Note: This is currently Linux-specific!
 class Stopwatch {
   //! Start of measurement
-  struct timeval start_time;
+  std::chrono::_V2::steady_clock::time_point start_time;
 
   //! Time difference between the last start and stop
   float diff_time;
@@ -70,8 +60,7 @@ private:
 //! Start time measurement
 ////////////////////////////////////////////////////////////////////////////////
 inline void Stopwatch::start() {
-
-  gettimeofday(&start_time, 0);
+  start_time = std::chrono::steady_clock::now();
   running = true;
 }
 
@@ -80,7 +69,6 @@ inline void Stopwatch::start() {
 //! variable. Also increment the number of times this clock has been run.
 ////////////////////////////////////////////////////////////////////////////////
 inline void Stopwatch::stop() {
-
   diff_time = getDiffTime();
   total_time += diff_time;
   running = false;
@@ -96,7 +84,7 @@ inline void Stopwatch::reset() {
   total_time = 0;
   clock_sessions = 0;
   if (running)
-    gettimeofday(&start_time, 0);
+    std::chrono::steady_clock::now();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -128,12 +116,11 @@ inline float Stopwatch::getAverageTime() const {
 
 ////////////////////////////////////////////////////////////////////////////////
 inline float Stopwatch::getDiffTime() const {
-  struct timeval t_time;
-  gettimeofday(&t_time, 0);
+  auto now = std::chrono::steady_clock::now();
 
   // time difference in milli-seconds
-  return (float)(1000.0 * (t_time.tv_sec - start_time.tv_sec) +
-                 (0.001 * (t_time.tv_usec - start_time.tv_usec)));
+  return (float)(1000.0 * (now.tv_sec - start_time.tv_sec) +
+                 (0.001 * (now.tv_usec - start_time.tv_usec)));
 }
 
-#endif // _STOPWATCH_H
+#endif // STOPWATCH_H
